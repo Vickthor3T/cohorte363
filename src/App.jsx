017c49ab9045e6c23ad18637b0952c9eb0c02c26
Victor Solis <victor.cardoza@tripleten-team.com>
2026-07-07 17:58:47 -0600

    fix: remove loginform

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import LoginForm from "./components/loginForm";
 import Header from "./components/Header/Header";
 import { Route, Routes } from "react-router-dom";
-import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import Protegido from "./components/Header/Protegido";
 import ProtectedRoute from "./components/ProtectedRoute";
